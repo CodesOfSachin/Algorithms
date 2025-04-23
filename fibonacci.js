5390f3a56fe3afba2a56bbfeab0068b9a6227060
CodesOfSachin <codes.of.sachin@gmail.com>
2025-04-23 14:44:55 +0530

    Fibonacci algorithm

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+    const fib = [0,1];
+    
+    for(let i = 2; i < n; i++) {
+        console.log(`I value is: ${i}`);
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    
+    return fib;
+}
+
+console.log(fibonacci(3)); // [0,1,1]
+console.log(fibonacci(5)); // [0,1,1,2,3]
